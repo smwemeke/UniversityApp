4982b611e3b5c5db4c95db229b506a717560b4e3
Suzan Mwemeke <sue.mwemeke@gmail.com>
2024-11-04 22:23:03 -0600

    University App

---
+++ UniversityApp/src/main/java/edu/miu/cs489/universityapp/UniversityAppApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs489.universityapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityAppApplication.class, args);
+    }
+
+}
---
+++ UniversityApp/src/test/java/edu/miu/cs489/universityapp/UniversityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs489.universityapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
